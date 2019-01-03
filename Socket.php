57e52c3bc94afb06cfeceedb6dff0d8f80698c7c
Zeno Li <zubin@xinmem.com>
2019-01-03 11:39:53 +0800

    review

--- Socket.php
+++ Socket.php
@@ -6,8 +6,6 @@
  * Time: 15:24
  */
 
-namespace Union\Utils;
-
 
 /**
  * socket 创建失败
@@ -29,10 +27,6 @@ define('ERROR_SOCKET_WRITE_FAIL', 5004);
  */
 define('ERROR_SOCKET_READ_FAIL', 5005);
 
-/**
- * 设置选项失败
- */
-define('ERROR_SOCKET_SETUP_FAIL', 5006);
 
 class Socket {
 
@@ -62,12 +56,6 @@ class Socket {
     protected $result = '';
 
     /**
-     * @var string
-     */
-    protected $service_sn = '';
-
-
-    /**
      * Socket  constructor.
      * @param  $config
      * @throws \Exception
@@ -77,41 +65,21 @@ class Socket {
         if($this->socket === false){
             throw new \Exception(socket_strerror(socket_last_error()), ERROR_SOCKET_CREATE_FAIL);
         }
-        //set send data timeout at 5s
-        if(!socket_set_option($this->socket, SOL_SOCKET, SO_SNDTIMEO, ['sec' => 5, 'usec' => 0])){
-            throw new \Exception('socket set option failed', ERROR_SOCKET_SETUP_FAIL);
-        }
 
         if(false === socket_connect($this->socket, $config->host, $config->port)){
             throw new \Exception(socket_strerror(socket_last_error()), ERROR_SOCKET_CONNECT_FAIL);
         }
-
-        $this->setServiceSn();
     }
 
-    /**
-     * 设置序列
-     */
-    protected function setServiceSn(){
-        $this->service_sn = '1590204000'. date('m') .'9' . self::getServiceSNSuffix();
-    }
 
     /**
      * 报文头
      *
-     * @param $serviceId
+     * @param  array  $header
      * @return $this
      */
-    protected function setHeader($serviceId){
-        $this->header = [
-            'service_sn' => $this->service_sn,
-            'requester_id' => '8110204',
-            'channel_id' => '49',
-            'service_time' => date('YmdHis'),
-            'version_id' => '01',
-            'service_id' => $serviceId,
-            'branch_id' => '811777777'
-        ];
+    protected function setHeader($header){
+        $this->header = $header;
 
         return $this;
     }
@@ -134,23 +102,21 @@ class Socket {
      * @return $this
      */
     protected function makeXML(){
-        $xml = '<?xml version="1.0" encoding="UTF-8"?><Service>';
-        $xml .= '<Service_Header>';
+        $this->xml = '<?xml version="1.0" encoding="UTF-8"?><Service>';
+        $this->xml .= '<Service_Header>';
         foreach ($this->header as $key => $value) {
-            $xml .= '<' . $key . '>' . $value . '</' . $key . '>';
+            $this->xml .= '<' . $key . '>' . $value . '</' . $key . '>';
         }
-        $xml .= '</Service_Header>';
-        $xml .= '<Service_Body>';
-        $xml .= '<request>';
+        $this->xml .= '</Service_Header>';
+        $this->xml .= '<Service_Body>';
+        $this->xml .= '<request>';
 
         foreach ($this->body as $key => $value) {
-            $xml .= '<' . $key . '>' . $value . '</' . $key . '>';
+            $this->xml .= '<' . $key . '>' . $value . '</' . $key . '>';
         }
-        $xml .= '</request>';
-        $xml .= '</Service_Body>';
-        $xml .= '</Service>';
-
-        $this->xml = $xml;
+        $this->xml .= '</request>';
+        $this->xml .= '</Service_Body>';
+        $this->xml .= '</Service>';
 
         return $this;
     }
@@ -163,8 +129,6 @@ class Socket {
      */
     protected function write(){
         $buffer = str_pad(strlen($this->xml), 7, '0', STR_PAD_LEFT) . $this->xml;
-        \Functions::log('request: ', 'socket.log');
-        \Functions::log($buffer, 'socket.log');
         $ret = socket_write($this->socket, $buffer, strlen($buffer));
         if($ret === false){
             throw new \Exception(socket_strerror(socket_last_error($this->socket)), ERROR_SOCKET_WRITE_FAIL);
@@ -193,16 +157,16 @@ class Socket {
     /**
      * 发送请求
      *
-     * @param $serviceId
+     * @param array $header
      * @param array $body
      * @throws \Exception
      */
-    public function send($serviceId, $body = array()){
-        $this->setHeader($serviceId)->setBody($body)->makeXML()->write();
+    public function send($header = array(), $body = array()){
+        $this->setHeader($header)->setBody($body)->makeXML()->write();
     }
 
     /**
-     * 获取原生结果
+     * 获取结果
      *
      * @return string
      * @throws \Exception
@@ -215,54 +179,6 @@ class Socket {
         return $this->result;
     }
 
-    /**
-     * 获取解析后的结果
-     *
-     * @return array
-     * @throws \Exception
-     */
-    public function getParseResult(){
-        if(empty($this->result)){
-            $this->read();
-        }
-
-        return Helper::parse(substr($this->result, 7));
-    }
-
-    /**
-     * 获取序号
-     *
-     * @return string
-     */
-    public function getServiceSN(){
-        return $this->service_sn;
-    }
-
-    /**
-     * 获取序列号
-     *
-     * @return string
-     */
-    private static function getServiceSNSuffix(){
-        $redis = \Phalcon\Di::getDefault()->get('redis');
-        if(!$redis->exists('t:service_sn')){
-            $redis->incr('t:service_sn');
-
-            return '000000';
-        }
-
-        $sn = $redis->get('t:service_sn');
-        if($sn > 999999){
-            $redis->set('t:service_sn', 1);
-
-            return '000000';
-        }
-
-        $redis->incr('t:service_sn');
-        $sn = str_pad($sn,6,'0',STR_PAD_LEFT);
-
-        return $sn;
-    }
 
     /**
      * 关闭链接
